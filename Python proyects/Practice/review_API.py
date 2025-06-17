6b6df2bc2fa9adbff3f56fe3782dc8cb23297dcc
ItsAlValenci <snowalbert@outlook.com>
2025-06-16 19:11:41 -0700

    update

--- Python proyects/Practice/review_API.py
+++ Python proyects/Practice/review_API.py
@@ -1,14 +1,34 @@
 import json
 import requests
 
-response = requests.get(url= "http://api.open-notify.org/iss-now.json")
+# response = requests.get(url= "http://api.open-notify.org/iss-now.json")
+
+# data = response.json()
+
+# longitud = data["iss_position"]['latitude']
+# latitud = data["iss_position"]['longitude']
+
+# location = (longitud,latitud)
+
+
+# print(location)
+
+MY_LATITUD = 37.790782
+MY_LONGITUD = -122.406325
+
+
+parameters = {
+    "lat": MY_LATITUD,
+    "lng": MY_LONGITUD,
+    "formatted": 0
+}
+
+response = requests.get(url="https://api.sunrise-sunset.org/json", params= parameters)
+response.raise_for_status()
 
 data = response.json()
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
 
-longitud = data["iss_position"]['latitude']
-latitud = data["iss_position"]['longitude']
-
-location = (longitud,latitud)
-
-
-print(location)
+print(sunrise)
+print(sunset)
